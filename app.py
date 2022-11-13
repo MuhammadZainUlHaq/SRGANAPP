757912d259ddf3ce5545479bc36eda9a2a715de9
Zain Ul haq <zain.18j2000@gmail.com>
2022-11-13 05:16:51 +0500

    make it better

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template, send_file, request
+import cv2
+import os
+import threading
+import time
+
+
 app= Flask(__name__)
 @app.route('/')
 def index():
-  return "<h1>Welcome to CodingX</h1>"
+  try:
+    return render_template("index.html", image_file='static/default.jpg', disable=True, msg="")
+  except:
+    return "<h1>Sorry, Something Went Wrong. Please Try Again Later<h1>"
+
+
