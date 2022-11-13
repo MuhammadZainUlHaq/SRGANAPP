aab47063e8dc6adfd53142061a5ed8e07b322b51
Zain Ul haq <zain.18j2000@gmail.com>
2022-11-13 06:06:10 +0500

    make it better

--- appl.py
+++ appl.py
@@ -8,15 +8,13 @@ import time
 app= Flask(__name__)
 @app.route('/')
 def index():
-    return render_template("index.html", image_file = 'static/default.jpg', disable = True, msg = "")
+    return render_template("new.html", image_file = 'static/default.jpg', disable = True, msg = "")
 
-@app.route('/download')
-def download():
-    return "nothing"
+
 
 @app.route('/',methods=['GET','POST'])
 def convert():
-    return render_template("index.html")
+    return render_template("new.html",disable = True)
 
 
 
